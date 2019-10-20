79ace9898f59dc4ac2f8a3510ca33cb656505aac
cule219 <culaficstefan@gmail.com>
2019-10-20 16:44:03 +0200

    build added

---
+++ client/build/precache-manifest.778c6d084a45655ec5b4841798538791.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40ace8cb7e2ca3deccb693668c796780",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bccac022457c0061eb7e",
+    "url": "/static/css/main.4d7d88ef.chunk.css"
+  },
+  {
+    "revision": "e9068a1c10b5f90861fc",
+    "url": "/static/js/2.48db07a2.chunk.js"
+  },
+  {
+    "revision": "bccac022457c0061eb7e",
+    "url": "/static/js/main.887dfb09.chunk.js"
+  },
+  {
+    "revision": "13fc4a53b97ddee7ef57",
+    "url": "/static/js/runtime-main.87bec749.js"
+  }
+]);
